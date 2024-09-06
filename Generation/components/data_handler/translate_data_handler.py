bc4c6b63e309545707628f1fa55718106d0a0b98
Jayanta47 <57296595+Jayanta47@users.noreply.github.com>
2024-09-06 16:19:38 +0600

    removed all pycache folders

--- Generation/components/data_handler/translate_data_handler.py
+++ Generation/components/data_handler/translate_data_handler.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-from .config_reader import ConfigReader
+from components.data_handler.config_reader import ConfigReader
 
 
 def sanitize_model_name(model_name: str):
@@ -15,12 +15,48 @@ def sanitize_model_name(model_name: str):
 class TranslationDataHandler:
     def __init__(self, config):
         self.config_reader = ConfigReader(config)
+        self.__setup_result_configurations()
 
     def get_attribute(self, attribute):
         return self.config[attribute]
 
-    def __create_valid_data_points(self):
-        pass
+    def __is_datapoint_eligible(self, index):
+        index_folder_path = os.path.join(self.model_folder_path, str(index))
+        return not os.path.exists(index_folder_path)
+
+    def __create_valid_data_points(self) -> dict:
+        import pandas as pd
+
+        refined_data_path = os.path.join("Data", "RefinedData", "data.csv")
+        refined_data = pd.read_csv(refined_data_path)
+
+        mask = [self.__is_datapoint_eligible(index) for index in refined_data.index]
+        valid_data_points = refined_data[mask].to_dict("records")
+
+        return valid_data_points
+
+    def __setup_result_configurations(self):
+        # Get the model name and storage folder path from the config reader
+        model_name = self.config_reader.get_attribute("model")
+        storage_folder_path = self.config_reader.get_attribute("storage_folder_path")
+        task = self.config_reader.get_attribute("task")
+        source_language = self.config_reader.get_attribute("source_language")
+        target_language = self.config_reader.get_attribute("target_language")
+
+        # Sanitize the model name
+        sanitized_model_name = sanitize_model_name(model_name)
+        result_folder_name = (
+            f"{sanitized_model_name}_{task}_{source_language}_{target_language}"
+        )
+
+        # Create the storage folder if it doesn't exist
+        if not os.path.exists(storage_folder_path):
+            os.makedirs(storage_folder_path)
+
+        # Create the model folder if it doesn't exist
+        self.model_folder_path = os.path.join(storage_folder_path, result_folder_name)
+        if not os.path.exists(self.model_folder_path):
+            os.makedirs(self.model_folder_path)
 
     def return_data_point(self, total=-1):
         valid_data_points = self.__create_valid_data_points()
@@ -32,24 +68,8 @@ class TranslationDataHandler:
 
     def save_data_point(self, index: int, content: dict):
 
-        # Get the model name and storage folder path from the config reader
-        model_name = self.config_reader.get_attribute("model")
-        storage_folder_path = self.config_reader.get_attribute("storage_folder_path")
-
-        # Sanitize the model name
-        sanitized_model_name = sanitize_model_name(model_name)
-
-        # Create the storage folder if it doesn't exist
-        if not os.path.exists(storage_folder_path):
-            os.makedirs(storage_folder_path)
-
-        # Create the model folder if it doesn't exist
-        model_folder_path = os.path.join(storage_folder_path, sanitized_model_name)
-        if not os.path.exists(model_folder_path):
-            os.makedirs(model_folder_path)
-
         # Create the index folder
-        index_folder_path = os.path.join(model_folder_path, str(index))
+        index_folder_path = os.path.join(self.model_folder_path, str(index))
         if not os.path.exists(index_folder_path):
             os.makedirs(index_folder_path)
 
@@ -63,3 +83,17 @@ class TranslationDataHandler:
             except Exception as e:
                 logger.error(f"Error occurred while writing to file: {e}\n")
                 pass  # Skip the operation if an error occurs
+
+
+if __name__ == "__main__":
+    data_handler = TranslationDataHandler(config="test.yaml")
+    print(data_handler.get_attribute("model"))
+    print(data_handler.get_attribute("task"))
+    print(data_handler.get_attribute("source_language"))
+    print(data_handler.get_attribute("target_language"))
+    print(data_handler.get_attribute("storage_folder_path"))
+    print(data_handler.model_folder_path)
+
+    for data_point in data_handler.return_data_point():
+        print(data_point)
+
